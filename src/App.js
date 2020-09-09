d7f3c3286b7ff368e2b4578b43eb4eee71983ddb
Vish Mayer <vish.mayer@gmail.com>
2020-09-09 17:14:25 +0100

    react component - refactor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Notes Application</h1>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Notes Application</h1>
+
+      </div>
+    );
+  }
 }
 
 export default App;
