708b57f3217b9d7a999f68211e09deda6dd386e0
Vish Mayer <vish.mayer@gmail.com>
2020-09-09 17:09:14 +0100

    enzyme setup - reconfigured test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,21 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+describe('Notes', () => {
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  test('renders title of application', () => {
+    expect(wrapper.find("h1").text()).toContain("Notes Application")
+  });
 });
+
